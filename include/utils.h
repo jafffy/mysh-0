93204f6985faa7a471de6eec0d94751b6d8b5f66
Jaewon Choi <jaewon.james.choi@gmail.com>
2017-09-16 23:39:18 +0900

    Add testing functionality

---
+++ include/utils.h
@@ -0,0 +1,7 @@
+#ifndef MYSH_UTILS_H_
+#define MYSH_UTILS_H_
+
+void mysh_parse_command(const char* command,
+                        int *argc, char** argv);
+
+#endif // MYSH_UTILS_H_
--- tests/src/command_parsing_test.cc
+++ tests/src/command_parsing_test.cc
@@ -1,5 +1,28 @@
+extern "C" {
+#include "utils.h"
+}
+
 #include "gtest/gtest.h"
 
-TEST(CommandParsingTest, Basic) {
-  EXPECT_EQ(1, 1);
+#include <stdlib.h>
+
+TEST(CommandParsingTest, BasicCommand) {
+  char** argv;
+  int argc = -1;
+
+  argv = (char**)malloc(2);
+  for (int i = 0; i < 2; ++i) {
+    argv[i] = (char*)malloc(512);
+  }
+
+  mysh_parse_command("cd test", &argc, argv);
+
+  EXPECT_EQ(argc, 2);
+  EXPECT_STREQ(argv[0], "cd");
+  EXPECT_STREQ(argv[1], "test");
+
+  for (int i = 0; i < 2; ++i) {
+    free(argv[i]);
+  }
+  free(argv);
 }
