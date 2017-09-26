4e38fda9861bea6aa4534ec580633ed83f5bb65d
spectaclehong <spectaclehong@gmail.com>
2017-09-27 00:23:16 +0900

    almost done

--- src/commands.c
+++ src/commands.c
@@ -1,5 +1,6 @@
 #include <string.h>
-
+#include <stdio.h>
+#include <unistd.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
@@ -7,6 +8,7 @@ int do_cd(int argc, char** argv) {
     return -1;
 
   // TODO: Fill it!
+  chdir(argv[1]);
   return 0;
 }
 
@@ -15,16 +17,20 @@ int do_pwd(int argc, char** argv) {
     return -1;
 
   // TODO: Fill it!
-
+  char output[200];
+  getcwd(output,200);
+  printf("%s\n",output);
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  if (strcmp(argv[0],"cd")==0 && argc>=2)
+    return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  if(strcmp(argv[0],"pwd")==0)
+    return 1;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,40 @@
 #include "utils.h"
-
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
+  int n=strlen(command);
+  *argc=0;
+  int m=0,j=0,cnt=0;
+  char *tmp=(char*)malloc(4*n);
+  for(int i=0;i<=n;i++){
+    if(command[i]==' '|| command[i]=='\n') tmp[m++]=0;
+    else if(command[i]=='|'){
+      tmp[m++]=0; tmp[m++]='|'; tmp[m++]=0;
+    }
+    else tmp[m++]=command[i];
+  }
+  while(j<m){
+    int sz=strlen(tmp+j);
+    if(!sz) j++;
+    else{
+      cnt++;
+      j+=sz+1;
+    }
+  }
+  *argv=(char**)malloc(4*cnt);
+  j=0;
+  while(j<m){
+    int sz=strlen(tmp+j);
+    if(!sz) j++;
+    else{
+      (*argv)[*argc]=(char*)malloc(2*(sz+1));
+      sprintf((*argv)[(*argc)++],"%s",tmp+j);
+      j+=sz+1;
+    }
+  }
+  free(tmp);
 }
