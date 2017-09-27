521c8e8c1dc70af424a39e2bed620c35563a18f4
spectaclehong <spectaclehong@gmail.com>
2017-09-27 12:12:51 +0900

    done except empty input

--- src/commands.c
+++ src/commands.c
@@ -8,8 +8,7 @@ int do_cd(int argc, char** argv) {
     return -1;
 
   // TODO: Fill it!
-  chdir(argv[1]);
-  return 0;
+  return chdir(argv[1]);
 }
 
 int do_pwd(int argc, char** argv) {
@@ -18,19 +17,23 @@ int do_pwd(int argc, char** argv) {
 
   // TODO: Fill it!
   char output[200];
-  getcwd(output,200);
-  printf("%s\n",output);
-  return 0;
+  if(getcwd(output,200)!=NULL){
+    fprintf(stdout,"%s\n",output);
+    return 0;
+  }
+  return -1;
 }
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  if (strcmp(argv[0],"cd")==0 && argc>=2)
+  if (strcmp(argv[0],"cd")==0 && argc==2)
     return 1;
+  return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  if(strcmp(argv[0],"pwd")==0)
+  if(strcmp(argv[0],"pwd")==0 && argc==1)
     return 1;
+  return 0;
 }
--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include "commands.h"
 #include "utils.h"
 
-static void release_argv(int argc, char** argv);
+static void release_argv(int argc, char*** argv);
 
 int main()
 {
@@ -34,19 +34,20 @@ int main()
       fprintf(stderr, "%s: command not found\n", argv[0]);
     }
 release_and_continue:
-    release_argv(argc, argv);
+    release_argv(argc, &argv);
     continue;
 release_and_exit:
-    release_argv(argc, argv);
+    release_argv(argc, &argv);
     break;
   }
 
   return 0;
 }
 
-static void release_argv(int argc, char** argv) {
+static void release_argv(int argc, char*** argv) {
   for (int i = 0; i < argc; ++i) {
-    free(argv[i]);
+    free((*argv)[i]);
   }
-  free(argv);
+  free(*argv);
+  *argv=NULL;
 }
